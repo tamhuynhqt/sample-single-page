ae1c955b157336849ec38b33c9597c92b19e02a2
Tam Huynh <tamhuynhqt@gmail.com>
2018-06-22 15:48:56 +1000

    First commit

---
+++ src/scripts/app.js
@@ -0,0 +1,65 @@
+//@koala-prepend "vendors/jquery-3.3.1.min.js"
+//@koala-prepend "vendors/slick.min.js"
+
+  
+$(document).ready(function () {
+    $(".testimonials").slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        speed: 600,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+
+            }
+
+        ]
+    });
+    $(".techslide").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        speed: 9000,
+        autoplay: true,
+        autoplaySpeed: 0,
+        arrows: false,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+
+            }
+
+        ]
+    });
+    //Scroll event
+  $(window).scroll(function(){
+    var scrolled = $(window).scrollTop();
+    if (scrolled > 300) $('#topBtn').fadeIn();
+    if (scrolled < 300) $('#topBtn').fadeOut();
+  });
+  
+  //Click event
+  $('#topBtn').click(function () {
+    $("html, body").animate({ scrollTop: "0" },  500);
+  });
+
+  var map;
+  window.initMap= function() {
+      map = new google.maps.Map(document.getElementById('contact-map'), {
+        center: {lat: -37.8070, lng: 144.9554},
+        zoom: 8
+      });
+    }
+});
